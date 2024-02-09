2fe76079f79f038e1f9f45ddfda90e80f634693d
SurendharHi5 <surendharhi5@gmail.com>
2024-02-08 19:39:46 -0800

    jwt remove

--- jwt_Token/token.js
+++
@@ -1,8 +0,0 @@
-// const jwt = require("jsonwebtoken");
-// const dotenv = require("dotenv")
-// dotenv.config();
-
-// const generateToken = (user) => jwt.sign({id: user.id}, process.env.JWT_SECRET, { expiresIn: "2d"})
-
-
-// module.exports = generateToken
